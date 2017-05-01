6893c4f4183f0c4b076d227d3382aa94c72842b8
PZ <pzika@langmaster.cz>
2017-05-01 23:27:04 +0200

    start

---
+++ Fulltext/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Fulltext
+{
+	public class PhraseWord
+	{
+		[Key]
+		public int Id { get; set; } //internal unique ID
+		[MaxLength(26)]
+		public string Word { get; set; } // first two chars has ord value of 32 plus LANG code. First LANG code is DictLang, second is Phrase Lang. If DictLang==Lang => Phrase means source, otherwise translation.
+		public int PhraseId { get; set; } //ID of phrase
+	}
+
+	public class Phrase
+	{
+		[Key]
+		public int Id { get; set; } //unique ID
+		public string Text { get; set; }
+		public byte[] WordIdxs { get; set; } //pos a length of word in the Text. Word Breaking result.
+	}
+
+	public class BloggingContext : DbContext
+	{
+		public DbSet<PhraseWord> PhraseWords { get; set; }
+		public DbSet<Phrase> Phrases { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(@"Data Source=PZ-W8VIRTUAL\SQLEXPRESS;Initial Catalog=test;Integrated Security=True;");
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<PhraseWord>().HasIndex(p => p.Word);
+		}
+
+		public void recreate()
+		{
+			Database.EnsureDeleted();
+			Database.EnsureCreated();
+		}
+	}
+
+	public class Class1
+	{
+		protected void Page_Load()
+		{
+			var ctx = new BloggingContext();
+			ctx.recreate();
+			//var all = ctx.Blogs.ToArray();
+		}
+	}
+}
---
+++ SpellChecker/Class1.cs
@@ -0,0 +1,49 @@
+﻿namespace SpellChecker
+{
+	using System.Collections.Generic;
+	using System.Linq;
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public class Lib
+	{
+
+		const string otherLang = "ru-ru";
+
+		//https://blogs.msdn.microsoft.com/wpf/2015/10/29/wpf-in-net-4-6-1/
+		void Check(System.Windows.Controls.TextBox TextInput)
+		{
+			string actLang = TextInput.Language.IetfLanguageTag == "en-gb" ? otherLang : "en-gb";
+			TextInput.Language = System.Windows.Markup.XmlLanguage.GetLanguage(actLang);
+
+			int index = 0;
+
+			List<string> suggestions = new List<string>();
+
+			while ((index = TextInput.GetNextSpellingErrorCharacterIndex(index, System.Windows.Documents.LogicalDirection.Forward)) != -1)
+			{
+
+				string currentError = TextInput.Text.Substring(index, TextInput.GetSpellingErrorLength(index));
+
+
+				suggestions.Add(currentError);
+
+				foreach (string suggestion in TextInput.GetSpellingError(index).Suggestions)
+				{
+
+					suggestions.Add(suggestion);
+
+				}
+
+				//spellingErrors.Add(index, suggestions.ToArray());
+
+				index += currentError.Length;
+
+			}
+
+			var Errors = suggestions.DefaultIfEmpty().Aggregate((r, i) => r + "\r\n" + i);
+
+		}
+	}
+}
+
