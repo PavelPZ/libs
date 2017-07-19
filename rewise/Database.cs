3f87c3eefcf382f5808e80425b56a4c3bec49f83
PZ <pzika@langmaster.cz>
2017-07-19 11:11:13 +0200

    v

---
+++ rewise/Database.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database {
+  public class dm_fts_parser {
+    [Key]
+    public string display_term { get; set; }
+  }
+
+  public class FulltextContext : DbContext {
+    public DbSet<BookDBModel.PhraseSrcWord> PhraseSrcWords { get; set; }
+    public DbSet<BookDBModel.PhraseDestWord> PhraseDestWords { get; set; }
+    public DbSet<BookDBModel.PhraseSrc> PhraseSrcs { get; set; }
+    public DbSet<BookDBModel.PhraseDest> PhraseDests { get; set; }
+    public DbSet<BookDBModel.Book> Books { get; set; }
+    public DbSet<FulltextDBModel.dm_fts_parser> dm_fts_parsers { get; set; }
+    public DbSet<RewiseDBModel.FactWord> FactWords { get; set; }
+    public DbSet<RewiseDBModel.UserFact> UserFacts { get; set; }
+    public DbSet<RewiseDBModel.UserDict> UserDicts { get; set; }
+    public DbSet<RewiseDBModel.User> Users { get; set; }
+    //https://github.com/aspnet/EntityFramework/issues/245 register fake dm_fts_parser entity
+    public DbSet<dm_fts_parser> dm_fts_parser { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+      optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["RewiseDatabase"].ConnectionString);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+      BookDBModel.BookDBModelBuild.OnModelCreating(modelBuilder);
+      RewiseDBModel.RewiseDBModelBuild.OnModelCreating(modelBuilder);
+    }
+  }
+}
---
+++ rewise/RewiseDBModel.cs
@@ -0,0 +1,102 @@
+﻿using LangsLib;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+
+namespace RewiseDBModel {
+
+  public class FactWord : FulltextDBModel.FtxWord {
+
+    public bool IsSrc { get; set; }
+    //*** relations
+    public int FactRef { get; set; } //ID of PhraseLow, containing word
+    public int DictRef { get; set; } //ID of dictionary
+
+    public UserFact Fact { get; set; }
+    public UserDict Dict { get; set; }
+  }
+
+  public class UserFact {
+    [Key]
+    public long Id { get; set; } //internal unique ID
+    public string Data { get; set; } //JSON data s faktem
+
+    public int DictRef { get; set; }
+    public UserDict Dict { get; set; }
+    public ICollection<FactWord> SrcWords { get; set; }
+    public ICollection<FactWord> DestWords { get; set; }
+  }
+
+  public class UserDict {
+    [Key]
+    public int Id { get; set; } //internal unique ID
+    public byte SrcLang { get; set; }
+    public byte DestLang { get; set; }
+
+    public int UserRef { get; set; }
+    public User User { get; set; }
+    public ICollection<UserFact> Facts { get; set; }
+    public ICollection<FactWord> SrcWords { get; set; }
+    public ICollection<FactWord> DestWords { get; set; }
+  }
+
+  public class User {
+    [Key]
+    public int Id { get; set; } //internal unique ID
+    public ICollection<UserDict> Dicts { get; set; }
+  }
+
+  public static class RewiseDBModelBuild {
+
+    public static void OnModelCreating(ModelBuilder modelBuilder) {
+
+      modelBuilder.Entity<FactWord>().HasIndex(p => new { p.Word, p.IsSrc, p.DictRef });
+
+      modelBuilder.Entity<UserFact>()
+        .HasMany(c => c.SrcWords)
+        .WithOne(e => e.Fact)
+        .HasForeignKey(e => e.FactRef)
+        .IsRequired()
+        .OnDelete(DeleteBehavior.Cascade);
+
+      modelBuilder.Entity<UserFact>()
+        .HasMany(c => c.DestWords)
+        .WithOne(e => e.Fact)
+        .HasForeignKey(e => e.FactRef)
+        .IsRequired()
+        .OnDelete(DeleteBehavior.Cascade);
+
+      modelBuilder.Entity<UserDict>()
+        .HasMany(c => c.Facts)
+        .WithOne(e => e.Dict)
+        .HasForeignKey(e => e.DictRef)
+        .IsRequired()
+        .OnDelete(DeleteBehavior.Cascade);
+
+      modelBuilder.Entity<User>()
+        .HasMany(c => c.Dicts)
+        .WithOne(e => e.User)
+        .HasForeignKey(e => e.UserRef)
+        .IsRequired()
+        .OnDelete(DeleteBehavior.Cascade);
+    }
+  }
+
+  //Javascript definice faktu
+  public class Fact {
+    //rewise
+
+    //data
+    public Side srcData;
+    public Side destData;
+  }
+  public class Side {
+    public Langs lang;
+    public string text;
+    public int sound;
+  }
+
+}
